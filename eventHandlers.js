35afaa3d3f8099d042fa3e4833871c0e79e6e8d1
saho90 <oymansah@students.zhaw.ch>
2021-11-07 18:43:09 +0100

    events

--- eventHandlers.js
+++ eventHandlers.js
@@ -6,8 +6,8 @@ exports.registerEventHandlers = function (source) {
     source.addEventListener('MyEvent', handleMyEvent);
     // Register more event handlers here
     source.addEventListener('Trainingevent', handleTrainingevent);
-    source.addEventListener('MyEvent', handleMyEvent);
-    source.addEventListener('MyEvent', handleMyEvent);
+    source.addEventListener('Trainingwiederholungen', handleTrainingwiederholungen);
+    source.addEventListener('Trainingkadenz', handleTrainingkadenz);
     source.addEventListener('MyEvent', handleMyEvent);
 
 }
@@ -55,6 +55,52 @@ function handleTrainingevent(event) {
   
         // Log the event in the database
         logger.logOne("MyDB", "Trainingevent", data);
+
+        // send data to all connected clients
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+function handleTrainingwiederholungen(event) {
+    // read variables from the event
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    //var datetime = new Date(data.timestamp); // convert the timestamp to a Date object
+
+    try {        
+  
+        // Log the event in the database
+        logger.logOne("MyDB", "Trainingwiederholungen", data);
+
+        // send data to all connected clients
+        exports.sendEvent(data);
+    } catch (error) {
+        console.log("Could not handle event: " + JSON.stringify(event) + "\n");
+        console.log(error)
+    }
+}
+function handleTrainingkadenz(event) {
+    // read variables from the event
+    var data = {
+        eventName: event.type,
+        eventData: JSON.parse(event.data).data, // the value of the event
+        deviceId: JSON.parse(event.data).coreid,
+        timestamp: JSON.parse(event.data).published_at
+    };
+
+    //var datetime = new Date(data.timestamp); // convert the timestamp to a Date object
+
+    try {        
+  
+        // Log the event in the database
+        logger.logOne("MyDB", "Trainingkadenz", data);
 
         // send data to all connected clients
         exports.sendEvent(data);
